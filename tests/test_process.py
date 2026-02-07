fde186d5566262153a2f82be4b587402d9dfeadb
soulLeo77 <176849955+soulLeo77@users.noreply.github.com>
2026-02-07 14:08:17 -0500

    Test updated

--- tests/test_process.py
+++ tests/test_process.py
@@ -4,7 +4,7 @@ from aconsys.views.login.window import LoginWindow
 def test_go_to_compras_view(executable_file, username, password) -> None:
     login_window = LoginWindow(executable_file)
     main_window = login_window.login(username, password)
-    main_window.change_work_period()
+    main_window.change_work_period("12")
     success = main_window.register_purchase_one_by_one(
         receipt_number="01",
         supplier_number="20193681655",
@@ -17,9 +17,10 @@ def test_go_to_compras_view(executable_file, username, password) -> None:
         account_number="4211103",
         sale_value="106.0",
         has_detraction=True,
-        type_detraction="22",
+        type_detraction="003",
         payment_date="3/01/2024",
         constancia_number="12345678",
+        aconsys_date="01/12/2025",
     )
 
     print(success)
